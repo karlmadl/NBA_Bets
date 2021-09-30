7f6fb61e0e9da4b200748c768ce416f7e0288831
Karl Madl <karl_madl@yahoo.com>
2021-09-29 20:14:39 -0400

    changed to structure of merging dataframes to df_merged at once, need to include win/loss/push columns

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from pysbr import *
 from datetime import datetime, date, timedelta
 import pandas as pd
 from functools import reduce
-BOOK = 'Bovada'
 
+
+def swap_pairs(ls):
+    swapped_pairs = [None]*len(ls)
+    swapped_pairs[::2] = ls[1::2]
+    swapped_pairs[1::2] = ls[::2]
+
+    return swapped_pairs
+
+
+BOOK = 'Bovada'
 CURRENT_DATE = '2021-03-11' # str(date.today())
 
 dt = datetime.strptime(CURRENT_DATE, '%Y-%m-%d')
-
 nba = NBA()
 sb = Sportsbook()
 
@@ -21,78 +29,75 @@ ps = pointspreads.dataframe(e).rename(columns={'spread / total': 'spread'})
 ml = moneylines.dataframe(e).rename(columns={'american odds': 'moneyline'})
 to = totals.dataframe(e).rename(columns={'spread / total': 'total'})
 
-
 pss = ps.loc[:, ['participant full name', 'spread']]
 mls = ml.loc[:, ['participant full name', 'moneyline']]
 tos = to.loc[:, ['participant full name', 'total']]
 
-tos['participant full name'] = mls['participant full name'].tolist()
-data_frames = [pss, mls, tos]
-
-df_merged = reduce(lambda left,right: pd.merge(left,right,on=['participant full name'], how='outer'), data_frames)
 
 corrections = {
     "L.A. Clippers Clippers": "L.A. Clippers",
     "L.A. Lakers Lakers": "L.A. Lakers"
 }
 
-names = df_merged['participant full name'].tolist()
+names = mls['participant full name'].tolist()
 for index, name in enumerate(names):
     if name in corrections:
         names[index] = corrections[name]
 
-df_merged['participant full name'] = names
+data_frames = [pss, mls, tos]
 
-df_merged['home'] = [i % 2 == 0 for i in range(len(names))]
+for df in data_frames:
+    df['participant full name'] = names
 
-opponents = [None]*len(names)
-opponents[::2] = names[1::2]
-opponents[1::2] = names[::2]
+opponents = swap_pairs(names)
 
-df_merged['opponent'] = opponents
+pregame_info = pd.DataFrame(data={
+    'participant full name': names,
+    'date': [CURRENT_DATE]*len(names),
+    'opponent': opponents,
+    'home': [i % 2 == 0 for i in range(len(names))],
+    })
 
-date_col = [CURRENT_DATE]*len(names)
-
-df_merged['date'] = date_col
 
 scores = ml['participant score'].tolist()
 
-df_merged['points scored'] = scores
-opp_scores = [None]*len(names)
-opp_scores[::2] = scores[1::2]
-opp_scores[1::2] = scores[::2]
+postgame_info = pd.DataFrame(data={
+    'participant full name': names,
+    'points scored': scores,
+    'points allowed': swap_pairs(scores),
+    'total points scored': [scores[i] + swap_pairs(scores)[i] for i in range(len(names))]
+})
 
-df_merged['points allowed'] = opp_scores
 
-df_merged['total points scored'] = [scores[i] + opp_scores[i] for i in range(len(names))]
+dfs_to_merge = [pregame_info, pss, mls, tos, postgame_info]
 
-df_merged = df_merged[['participant full name', 'date', 'opponent', 'home', 'spread', 'moneyline', 'total', 'points scored', 'points allowed', 'total points scored']]
-
-ps_result = []
-for i in range(len(df_merged)):
-    if df_merged['points allowed'][i] - df_merged['points scored'][i] > df_merged['spread'][i]:
-        ps_result.append('loss')
-    elif df_merged['points allowed'][i] - df_merged['points scored'][i] < df_merged['spread'][i]:
-        ps_result.append('win')
-    elif df_merged['points allowed'][i] - df_merged['points scored'][i] == df_merged['spread'][i]:
-        ps_result.append('push')
-
-ml_result = []
-for i in range(len(df_merged)):
-    if df_merged['points allowed'][i] > df_merged['points scored'][i]:
-        ml_result.append('loss')
-    elif df_merged['points allowed'][i] < df_merged['points scored'][i]:
-        ml_result.append('win')
-
-to_result = []
-for i in range(len(df_merged)):
-    if df_merged['total points scored'][i] < df_merged['total'][i]:
-        to_result.append('loss')
-    elif df_merged['total points scored'][i] > df_merged['total'][i]:
-        to_result.append('win')
-    elif df_merged['total points scored'][i] == df_merged['total'][i]:
-        to_result.append('push')
-
-df_merged['spread result'], df_merged['moneyline result'], df_merged['total result'] = ps_result, ml_result, to_result
+df_merged = reduce(lambda left,right: pd.merge(left,right,on=['participant full name'], how='outer'), dfs_to_merge)
 
 print(df_merged)
+
+
+
+# ps_result, ml_result, to_result = [], [], []
+# for i in range(len(df_merged)):
+#     if df_merged['points allowed'][i] - df_merged['points scored'][i] > df_merged['spread'][i]:
+#         ps_result.append('loss')
+#     elif df_merged['points allowed'][i] - df_merged['points scored'][i] < df_merged['spread'][i]:
+#         ps_result.append('win')
+#     elif df_merged['points allowed'][i] - df_merged['points scored'][i] == df_merged['spread'][i]:
+#         ps_result.append('push')
+
+#     if df_merged['points allowed'][i] > df_merged['points scored'][i]:
+#         ml_result.append('loss')
+#     elif df_merged['points allowed'][i] < df_merged['points scored'][i]:
+#         ml_result.append('win')
+
+#     if df_merged['total points scored'][i] < df_merged['total'][i]:
+#         to_result.append('loss')
+#     elif df_merged['total points scored'][i] > df_merged['total'][i]:
+#         to_result.append('win')
+#     elif df_merged['total points scored'][i] == df_merged['total'][i]:
+#         to_result.append('push')
+
+# df_merged['spread result'], df_merged['moneyline result'], df_merged['total result'] = ps_result, ml_result, to_result
+
+# print(df_merged)
