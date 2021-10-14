95c21e0cc59b849223222367165c997d8f963fa3
Karl Madl <karl_madl@yahoo.com>
2021-10-14 12:55:49 -0400

    slightly polished up main.py which now handles data collection and upload; added a WIP ipynb for filtering dataframes more easily

--- main.py
+++ main.py
@@ -17,50 +17,49 @@ DATE = str(date.today() - timedelta(days=1))
 
 dt = datetime.strptime(DATE, '%Y-%m-%d')
 nba = NBA()
-sb = Sportsbook()
+sb = Sportsbook() 
 
 e = EventsByDate(nba.league_id, dt)
 
-moneylines = OpeningLines(e.ids(), nba.market_id('ml'), sb.id(BOOK))
-pointspreads = OpeningLines(e.ids(), nba.market_id('ps'), sb.id(BOOK))
-totals = OpeningLines(e.ids(), nba.market_id('total'), sb.id(BOOK))
+moneylines = OpeningLines(e.ids(), nba.market_id('ml'), sb.id(BOOK)).dataframe(e)
+pointspreads = OpeningLines(e.ids(), nba.market_id('ps'), sb.id(BOOK)).dataframe(e)
+totals = OpeningLines(e.ids(), nba.market_id('total'), sb.id(BOOK)).dataframe(e)
 
-ps = pointspreads.dataframe(e).rename(columns={'spread / total': 'spread'})
-ml = moneylines.dataframe(e).rename(columns={'american odds': 'moneyline'})
-to = totals.dataframe(e).rename(columns={'spread / total': 'total'})
-
-pss = ps.loc[:, ['participant full name', 'spread']]
-mls = ml.loc[:, ['participant full name', 'moneyline']]
-tos = to.loc[:, ['participant full name', 'total']]
-
+pointspreads.rename(columns={'spread / total': 'spread'}, inplace=True)
+moneylines.rename(columns={'american odds': 'moneyline'}, inplace=True)
+totals.rename(columns={'spread / total': 'total'}, inplace=True)
 
 corrections = {
     "L.A. Clippers Clippers": "L.A. Clippers",
     "L.A. Lakers Lakers": "L.A. Lakers"
 }
 
-names = mls['participant full name'].tolist()
+names = moneylines['participant full name'].tolist()
 for index, name in enumerate(names):
     if name in corrections:
         names[index] = corrections[name]
 
-data_frames = [pss, mls, tos]
+events = moneylines['event'].tolist()
+home_teams = [events[i].split(" ")[-1] for i in range(len(events))]
 
-for df in data_frames:
-    df['participant full name'] = names
 
-opponents = swap_pairs(names)
+
+
 
 pregame_info = pd.DataFrame(data={
     'participant full name': names,
     'date': [DATE]*len(names),
-    'opponent': opponents,
-    'home': [i % 2 == 0 for i in range(len(names))],
+    'opponent': swap_pairs(names),
+    'home': [home_teams[i] == names[i].split(" ")[1] for i in range(len(names))],
     })
 
 
-scores = ml['participant score'].tolist()
+pointspreads_to_merge = pointspreads.loc[:, ['participant full name', 'spread']]
+moneylines_to_merge = moneylines.loc[:, ['participant full name', 'moneyline']]
+totals_to_merge = totals.loc[:, ['participant full name', 'total']]
 
+
+scores = moneylines['participant score'].tolist()
 postgame_info = pd.DataFrame(data={
     'participant full name': names,
     'points scored': scores,
@@ -70,18 +69,24 @@ postgame_info = pd.DataFrame(data={
 })
 
 
-dfs_to_merge = [pregame_info, pss, mls, tos, postgame_info]
-
-df_merged = reduce(lambda left,right: pd.merge(left,right,on=['participant full name'], how='outer'), dfs_to_merge)
-
-results_df = pd.DataFrame(data={
+results = pd.DataFrame(data={
     'participant full name': names,
-    'spread result': ["win" if df_merged['lost by'][i] < df_merged['spread'][i] else "loss" if df_merged['lost by'][i] > df_merged['spread'][i] else "push" for i in range(len(names))],
-    'moneyline result': ["win" if df_merged["points allowed"][i] < df_merged['points scored'][i] else 'loss' for i in range(len(names))],
-    'total result': ["win" if df_merged['total points scored'][i] > df_merged['total'][i] else "loss" if df_merged['total points scored'][i] < df_merged['total'][i] else "push" for i in range(len(names))]
+    'spread result': ["win" if postgame_info['lost by'][i] < pointspreads_to_merge['spread'][i] else "loss" if postgame_info['lost by'][i] > pointspreads_to_merge['spread'][i] else "push" for i in range(len(names))],
+    'moneyline result': ["win" if postgame_info["points allowed"][i] < postgame_info['points scored'][i] else 'loss' for i in range(len(names))],
+    'total result': ["win" if postgame_info['total points scored'][i] > totals_to_merge['total'][i] else "loss" if postgame_info['total points scored'][i] < totals_to_merge['total'][i] else "push" for i in range(len(names))]
 })
 
-final_df = pd.merge(df_merged, results_df, on='participant full name')
+
+
+wrong_name_dfs = [pointspreads_to_merge, moneylines_to_merge, totals_to_merge]
+
+for df in wrong_name_dfs:
+    df['participant full name'] = names
+
+dfs_to_merge = [pregame_info, pointspreads_to_merge, moneylines_to_merge, totals_to_merge, postgame_info, results]
+
+final_df = reduce(lambda left,right: pd.merge(left,right,on=['participant full name'], how='outer'), dfs_to_merge)
+
 
 
 
@@ -93,10 +98,9 @@ from gspread_dataframe import set_with_dataframe
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    "../credentials.json", scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(credentials)
-sheet1 = client.open("NBA Bets").get_worksheet(3)
+sheet1 = client.open("NBA Bets 2021-22").get_worksheet(1)
 
 height = len(sheet1.col_values(1)) + 1
 
