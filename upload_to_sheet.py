da1a14aa648309087cfef386c970605e260cd56d
Karl Madl <karl_madl@yahoo.com>
2022-04-23 17:52:57 -0400

    rewrote how the connection to the sheet is created with more straightforward gspread (removed scope var)

--- upload_to_sheet.py
+++ upload_to_sheet.py
@@ -1,25 +1,28 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 from gspread_dataframe import set_with_dataframe
+from pandas import DataFrame
 
 
-def upload_to_google_sheet(data, workbook, sheet_index, credentials_json_path, row=None, col=1, head=False, flush_range=None):
-    scope = ["https://spreadsheets.google.com/feeds",
-             "https://www.googleapis.com/auth/spreadsheets",
-             "https://www.googleapis.com/auth/drive.file",
-             "https://www.googleapis.com/auth/drive"]
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_json_path, scope)
-    client = gspread.authorize(credentials)
-    sheet1 = client.open(workbook).get_worksheet(sheet_index)
+def upload_to_google_sheet(data: DataFrame,
+                           credentials_json_path: str,
+                           workbook: str,
+                           sheet_index: int = 0,
+                           row: int = None,
+                           col: int = 1,
+                           head: bool = False,
+                           flush_range: str = None):
+
+    gc = gspread.service_account(filename=credentials_json_path)
+    sheet = gc.open(workbook).get_worksheet(sheet_index)
 
     if flush_range is not None:
-        col_num, row_num = range_to_colrows(flush_range, sheet1.row_count)
-        sheet1.batch_update([{"range": flush_range, "values": [[""]*col_num]*row_num}])
+        col_num, row_num = range_to_colrows(flush_range, sheet.row_count)
+        sheet.batch_update([{"range": flush_range, "values": [[""]*col_num]*row_num}])
 
     if row is None:
-        row = len(sheet1.col_values(col)) + 1
+        row = len(sheet.col_values(col)) + 1
 
-    set_with_dataframe(sheet1, data, col=col, row=row, include_column_header=head)
+    set_with_dataframe(sheet, data, col=col, row=row, include_column_header=head)
 
 
 def range_to_colrows(range: str, sheet_length: int):
